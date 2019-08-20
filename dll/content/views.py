d11d66983f5ad96c7020f7b383036274cca11ab2
Robert Stein <robert@blueshoe.de>
2019-08-20 15:48:34 +0200

    - remove permission from public endpoints

--- dll/content/views.py
+++ dll/content/views.py
@@ -173,6 +173,7 @@ class ContentDataFilterView(ListAPIView):
     ]
     search_fields = ['name', 'teaser']
     model = None
+    permission_classes = []
 
     def get_queryset(self):
         qs = super(ContentDataFilterView, self).get_queryset().objects.instance_of(self.model)
