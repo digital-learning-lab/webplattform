732c4798ed7326d0d5c33dd739e5a2107f628765
Robert Stein <robert@blueshoe.de>
2019-08-20 16:23:44 +0200

    - set django debug to true for now

--- dll/static/src/js/ContentSubmissionApp.vue
+++ dll/static/src/js/ContentSubmissionApp.vue
@@ -3,6 +3,7 @@
     <div v-if="mode === 'create'">
       <app-text-input id="author" :read-only="true" label="Autor_in" :value.sync="data.author" :required="true"></app-text-input>
       <app-text-input id="title" label="Titel des Unterrichtsbausteins" :value.sync="data.title" :required="true" :character-counter="true" :maximal-chars="140"></app-text-input>
+      <app-dropdown id="imageRights" label="Bildrechte" :value.sync="data.imageRights" :required="true"></app-dropdown>
       <app-text-area id="teaser" label="Teaser" :required="true" :value.sync="data.teaser"></app-text-area>
       <app-text-area id="description" label="Detaillierte Beschreibung" :required="true" :value.sync="data.teaser" :character-counter="true" :maximal-chars="1800"></app-text-area>
       <app-text-area id="estimatedTime" label="Zeitumfang der Durchführung" :value.sync="data.estimatedTime" :character-counter="true" :maximal-chars="250"></app-text-area>
@@ -14,10 +15,12 @@
 
   import TextInput from './components/TextInput.vue'
   import TextArea from './components/TextArea.vue'
+  import Dropdown from './components/Dropdown.vue'
 
   export default {
     name: 'ContentSubmissionApp',
     components: {
+      'AppDropdown': Dropdown,
       'AppTextInput': TextInput,
       'AppTextArea': TextArea
     },
---
+++ dll/static/src/js/components/Dropdown.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="form-group">
+    <label :for="id">{{ label }}:<span v-if="required">*</span></label>
+    <v-select v-model="inputValue" class="form-control"></v-select>
+    <small v-if="characterCounter" class="form-text text-muted float-right">{{ charactersLeft }} Zeichen verbleibend</small>
+  </div>
+</template>
+
+<script>
+  import vSelect from 'vue-select'
+
+  export default {
+    name: 'Dropdown',
+    components: {
+      'v-select': vSelect
+    },
+    props: {
+      id: {
+        type: String,
+        default: '',
+        required: true
+      },
+      type: {
+        type: String,
+        default: 'text',
+        required: false
+      },
+      readOnly: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      placeholder:{
+        type: String,
+        default: '',
+        required: false
+      },
+      characterCounter: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      maximalChars: {
+        type: Number,
+        default: 0,
+        required: false
+      },
+      required: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      label: {
+        type: String,
+        default: '',
+        required: true
+      },
+      value: {
+        type: String,
+        default: '',
+        required: false
+      }
+    },
+    computed: {
+      charactersLeft () {
+        return this.maximalChars - this.value.length
+      }
+    },
+    data () {
+      return {
+        inputValue: ''
+      }
+    },
+    created () {
+      this.inputValue = this.value
+    },
+    watch: {
+      inputValue (newValue) {
+        this.$emit('update:value', newValue)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
