41f68e7254476d447f1485147a7c539c04f0744b
Robert Stein <robert@blueshoe.de>
2019-08-20 15:00:35 +0200

    stage commit

--- dll/static/src/js/ContentSubmissionApp.vue
+++ dll/static/src/js/ContentSubmissionApp.vue
@@ -1,13 +1,20 @@
 <template>
   <form class="mb-4">
-
+    <div v-if="mode === 'create'">
+      123
+    </div>
   </form>
 </template>
 
 <script>
 
   export default {
-    name: 'ContentSubmissionApp'
+    name: 'ContentSubmissionApp',
+    data () {
+      return {
+        mode: 'create'
+      }
+    }
   }
 </script>
 
---
+++ dll/static/src/js/components/TextInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="form-group">
+    <label :for="id">Email address</label>
+    <input type="email" class="form-control" :id="id" :placeholder="placeholder">
+    <small v-if="characterCounter" class="form-text text-muted">{{ charactersLeft }} Zeichen verbleibend</small>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TextInput',
+    props: {
+      id: {
+        type: String,
+        default: '',
+        required: true
+      },
+      placeholder:{
+        type: String,
+        default: '',
+        required: false
+      },
+      characterCounter: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    computed: {
+      charactersLeft () {
+        return 1
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
