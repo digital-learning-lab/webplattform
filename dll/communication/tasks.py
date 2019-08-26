8095bf71e729f02f1168a4f8ae073fb1241385b9
victor <victor@blueshoe.de>
2019-08-26 11:32:37 +0200

    celery setup, changed send_mail to celery task

--- dll/communication/tasks.py
+++ dll/communication/tasks.py
@@ -2,14 +2,15 @@ import logging
 
 from dll.communication.models import CommunicationEventType
 from dll.communication.utils import Dispatcher
+from dll.configuration.celery import app
 from dll.user.models import DllUser
 
 logger = logging.getLogger('dll.communication.tasks')
 
 
-def send_mail(event_type_code, ctx={}, recipient_ids=None, sender_id=None, email=None, cc=None, bcc=None):
-    _retry = 1  # self.request.retries
-    logger.debug('Email sending initiated - attempt nr. {}'.format(_retry))
+@app.task(bind=True, default_retry_delay=30*60, max_retries=3)
+def send_mail(self, event_type_code, ctx={}, recipient_ids=None, sender_id=None, email=None, cc=None, bcc=None):
+    logger.debug('Email sending initiated - attempt nr. {}'.format(self.request.retries))
     if not (recipient_ids or email):
         raise ValueError('You must provide either a recipient or an email')
 
