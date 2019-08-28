01503566ab76609899d524dce29e5e598ae7e62b
victor <victor@blueshoe.de>
2019-08-28 12:33:17 +0200

    added new index fields

--- dll/content/search_indexes.py
+++ dll/content/search_indexes.py
@@ -5,7 +5,10 @@ from dll.content.models import Content
 
 class ContentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name')
+    name = indexes.CharField(model_attr='name', boost=2)
+    teaser = indexes.CharField(model_attr='teaser', boost=1.5)
+    additional_info = indexes.CharField(model_attr='additional_info', boost=1)
+    tags = indexes.MultiValueField()
     published = indexes.DateTimeField(model_attr='created')
 
     def get_model(self):
@@ -13,3 +16,6 @@ class ContentIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         return self.get_model().objects.published()
+
+    def prepare_tags(self, obj):
+        return list(obj.tags.values_list('name', flat=True))
