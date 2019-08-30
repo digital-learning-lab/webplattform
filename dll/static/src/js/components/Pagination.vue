7a7d1b4e67f9d89ebe3db4992cf5f9c8784a4ff1
Robert Stein <robert@blueshoe.de>
2019-08-30 16:39:00 +0200

    - reduce pagination buttons

--- dll/static/src/js/components/Pagination.vue
+++ dll/static/src/js/components/Pagination.vue
@@ -3,7 +3,11 @@
     <button class="pagination__button pagination__previous" @click="$emit('prev')" :disabled="pagination.prev === null">
       <span><</span>
     </button>
-    <button class="pagination__button pagination__number" :class="{'pagination__button--active': page === currentPage}" v-for="page in pages" @click="$emit('jump', $event, page)">{{ page }}</button>
+    <div v-for="(page, index) in pages" class="pagination__number">
+      <button class="pagination__button" disabled v-if="pages[index - 1] !== page - 1 && page > 1">...</button>
+      <button class="pagination__button" :disabled="page === '...'" :class="{'pagination__button--active': page === currentPage}" @click="$emit('jump', $event, page)">{{page}}</button>
+      <button class="pagination__button" disabled v-if="pages.length < 4 && pages[index + 1] !== page + 1 && page < pages.length - 1">...</button>
+    </div>
     <button class="pagination__button pagination__next" @click="$emit('next')" :disabled="pagination.next === null">
       <span>></span>
     </button>
@@ -23,6 +27,16 @@
         default () {
           return {}
         }
+      },
+      paginateBy: {
+        type: Number,
+        default: 10,
+        required: false
+      },
+      margin: {
+        type: Number,
+        default: 2,
+        required: false
       }
     },
     methods: {},
@@ -33,8 +47,11 @@
           let pages = []
           let page = 1
           while (counter > 0) {
-            pages.push(page++)
-            counter -= 10
+            if (page === 1 || page === Math.ceil(this.pagination.count / this.paginateBy) || (page >= this.currentPage - this.margin && page <= this.currentPage + this.margin)) {
+              pages.push(page)
+            }
+            page++
+            counter -= this.paginateBy
           }
           return pages
         }
