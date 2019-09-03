e8b0f57d27f406c1e5a7ea86914ef0aa7b39e92c
victor <victor@blueshoe.de>
2019-09-03 14:16:11 +0200

    added newsletter subscription export

--- dll/communication/admin.py
+++ dll/communication/admin.py
@@ -1,4 +1,8 @@
+import csv
+
+from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
 
 from dll.communication.models import CommunicationEvent, CommunicationEventType, NewsletterSubscrption
 
@@ -9,3 +13,25 @@ admin.site.register(CommunicationEventType)
 @admin.register(NewsletterSubscrption)
 class NewsletterSubscriptionAdmin(admin.ModelAdmin):
     list_display = ('email', 'doi_confirmed')
+    change_list_template = "admin/newsletter_subscription_changelist.html"
+
+    def get_urls(self):
+        urls = super(NewsletterSubscriptionAdmin, self).get_urls()
+        custom_urls = [
+            url(r'export-subscriptions/', self.export_subscriptions)
+        ]
+        return custom_urls + urls
+
+    def export_subscriptions(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="newsletter_subscriptions.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['id', 'email', 'confirmation_date'])
+        for sub in self.get_queryset(request).filter(doi_confirmed=True):
+            if sub.doi_confirmed_date:
+                date = sub.doi_confirmed_date.strftime("%d-%m-%Y")
+            else:
+                # just in case the subscription was added manually in the admin
+                date = ''
+            writer.writerow([sub.pk, sub.email, date])
+        return response
