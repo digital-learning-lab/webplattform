eec58798887e8ef239e9827ea110d139a41fabd4
Robert Stein <robert@blueshoe.de>
2019-09-03 10:31:05 +0200

    - remove logging statement

--- dll/static/src/js/components/LinksInput.vue
+++ dll/static/src/js/components/LinksInput.vue
@@ -100,14 +100,12 @@
       },
       isValidUrl (url) {
         // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-        console.log('checking ' + url)
         var pattern = new RegExp('^(https?:\\/\\/)'+
           '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
           '((\\d{1,3}\\.){3}\\d{1,3}))'+
           '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
           '(\\?[;&a-z\\d%_.~+=-]*)?'+
           '(\\#[-a-z\\d_]*)?$','i');
-        console.log(!!pattern.test(url))
         return !!pattern.test(url)
       },
       checkLinkValid (link) {
