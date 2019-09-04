d1d649ef4d575ee5c65d7c1152079cb31446393a
Robert Stein <robert@blueshoe.de>
2019-09-04 17:06:27 +0200

    - fix overflow bug for sidebar - create subjects for staging env

---
+++ dll/content/management/commands/create_subjects.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from django.core.management import BaseCommand
+
+from dll.content.models import Subject
+
+SUBJECTS = [
+    'Arbeit und Beruf',
+    'Bildende Kunst',
+    'Biologie',
+    'Chemie',
+    'Deutsch',
+    'Deutsch als Zweitsprache',
+    'Englisch',
+    'Französisch',
+    'Fremdsprachen',
+    'Geographie',
+    'Geschichte',
+    'Gesellschaft',
+    'Informatik',
+    'Kunst',
+    'Latein',
+    'Mathematik',
+    'Musik',
+    'Naturwissenschaft und Technik',
+    'Niederdeutsch',
+    'Philosophie',
+    'Physik',
+    'Politik',
+    'Politik/Gesellschaft/Wirtschaft',
+    'Religion',
+    'Sachunterricht',
+    'Spanisch',
+    'Sport',
+    'Theater'
+]
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for subject in SUBJECTS:
+            Subject.objects.get_or_create(name=subject)
