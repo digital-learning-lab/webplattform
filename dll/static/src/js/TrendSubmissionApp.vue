116027b8f28507d30725df906a012ffc1d7a0df9
Robert Stein <robert@blueshoe.de>
2019-09-09 10:53:06 +0200

    - add image to required fields for trend submission

--- dll/static/src/js/TrendSubmissionApp.vue
+++ dll/static/src/js/TrendSubmissionApp.vue
@@ -3,7 +3,7 @@
       <app-text-input id="author" :readonly="true" label="Autor_in" :value.sync="data.author" :required="true"></app-text-input>
       <app-text-input id="title" :readonly="readonly" :review="review" :review-value.sync="reviewValue.name" :error="errorFields.includes('name')" label="Titel des Trends" :value.sync="data.name" :required="true" :character-counter="true" :maximal-chars="140" :help-text="getHelpText('name')"></app-text-input>
     <div v-if="mode === 'edit' || mode === 'review'">
-      <app-file-input id="image" :readonly="readonly" :review="review" :review-value.sync="reviewValue.image" :error="errorFields.includes('image')" label="Anzeigebild" file-label="Bild wählen" :value.sync="previewImage" :image="data.image" :help-text="getHelpText('image')" :hintText="imageHintText"></app-file-input>
+      <app-file-input id="image" :readonly="readonly" :review="review" :required="true" label="Anzeigebild" file-label="Bild wählen" :value.sync="previewImage" :review-value.sync="reviewValue.previewImage" :error="errorFields.includes('image')" :image="data.image" :help-text="getHelpText('image')" :hintText="imageHintText"></app-file-input>
       <app-text-area id="teaser" :readonly="readonly" :review="review" :review-value.sync="reviewValue.teaser" :error="errorFields.includes('teaser')" label="Kurzzusammenfassung" :required="true" :value.sync="data.teaser" :rows="3" :help-text="getHelpText('teaser')"></app-text-area>
       <app-dropdown id="co_authors" :readonly="readonly" :review="review" :review-value.sync="reviewValue.co_authors" :error="errorFields.includes('co_authors')" label="Co-Autor_innen" :value.sync="data.co_authors" fetch-url="/api/authors" :multiple="true" :help-text="getHelpText('co_authors')"></app-dropdown>
       <app-pending-co-authors :pending_co_authors="data.pending_co_authors"></app-pending-co-authors>
@@ -70,6 +70,7 @@
         requiredFields: [
         {field: 'name', title: 'Titel'},
         {field: 'teaser', title: 'Kurzzusammenfassung'},
+        {field: 'image', title: 'Anzeigebild'},
         {field: 'competences', title: 'Kompetenzen in der digitalen Welt'}
       ],
         categoryOptions: [
