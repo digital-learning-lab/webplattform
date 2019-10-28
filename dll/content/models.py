2a6fc51464791c65145feb2bee6acb250f74713c
MH <michael.heinemann@tuhh.de>
2019-10-28 10:38:10 +0100

    change database fields

--- dll/content/models.py
+++ dll/content/models.py
@@ -56,14 +56,14 @@ class Content(ModelMeta, RulesModelMixin, PublisherModel, PolymorphicModel):
                                         verbose_name=_("Kollaborateure"), blank=True)
     image = FilerImageField(on_delete=models.SET_NULL, null=True, verbose_name=_('Anzeigebild'))
     teaser = models.TextField(max_length=140, verbose_name=_("Teaser"), null=True, blank=True)
-    learning_goals = ArrayField(models.CharField(max_length=400), verbose_name=_("Lernziele"), default=list, null=True,
+    learning_goals = ArrayField(models.CharField(max_length=850), verbose_name=_("Lernziele"), default=list, null=True,
                                 blank=True)
     related_content = models.ManyToManyField('self', verbose_name=_("Verwandte Tools/Trends/Unterrichtsbausteine"),
                                              blank=True)
     view_count = models.PositiveIntegerField(default=0, editable=False)
     base_folder = models.CharField(max_length=100, null=True, editable=False)
     # additional_info: 'hinweise' for ubausteine, 'anmerkung'  for tools, 'hintergrund' for Trends
-    additional_info = models.TextField(_("Hinweise/Anmerkungen/Hintergrund"), max_length=1500, blank=True, null=True)
+    additional_info = models.TextField(_("Hinweise/Anmerkungen/Hintergrund"), blank=True, null=True)
     competences = models.ManyToManyField('Competence', verbose_name=_("Kompetenzen"), blank=True)
     sub_competences = models.ManyToManyField('SubCompetence', verbose_name=_("Subkompetenzen"), blank=True)
     json_data = JSONField(default=dict)  # see README for details
@@ -331,17 +331,17 @@ class Content(ModelMeta, RulesModelMixin, PublisherModel, PolymorphicModel):
 
 class TeachingModule(Content):
     description = models.TextField(_("Beschreibung"), null=True, blank=True)
-    subject_of_tuition = ArrayField(models.CharField(max_length=400), verbose_name=_("Unterichtsgegenstand"),
+    subject_of_tuition = ArrayField(models.CharField(max_length=1800), verbose_name=_("Unterichtsgegenstand"),
                                     default=list, null=True, blank=True)
     educational_plan_reference = models.TextField(_("Bildungsplanbezug"), null=True, blank=True)
     school_class = IntegerRangeField(verbose_name=_("Jahrgangsstufe"), null=True, blank=True)
     # estimated time e.g. Doppelstunde,unterrichtsbegleitend
-    estimated_time = models.CharField(max_length=200, verbose_name=_("Zeitumfang"), null=True, blank=True)
-    equipment = ArrayField(models.CharField(max_length=200), verbose_name=_("Ausstattung"), default=list, null=True,
+    estimated_time = models.CharField(max_length=300, verbose_name=_("Zeitumfang"), null=True, blank=True)
+    equipment = ArrayField(models.CharField(max_length=350), verbose_name=_("Ausstattung"), default=list, null=True,
                            blank=True)
     state = models.CharField(_("Bundesland"), max_length=22, choices=GERMAN_STATES, null=True, blank=True)
-    differentiating_attribute = models.TextField(_("Differenzierung"), max_length=700, null=True, blank=True)
-    expertise = ArrayField(models.CharField(max_length=400), verbose_name=_("Fachkompetenzen"), default=list, null=True,
+    differentiating_attribute = models.TextField(_("Differenzierung"), null=True, blank=True)
+    expertise = ArrayField(models.CharField(max_length=1500), verbose_name=_("Fachkompetenzen"), default=list, null=True,
                            blank=True)
     subjects = models.ManyToManyField('Subject', verbose_name=_("Unterrichtsfach"), blank=True)
     school_types = models.ManyToManyField('SchoolType', verbose_name=_("Schulform"), blank=True)
@@ -415,8 +415,8 @@ class Tool(Content):
     status = models.CharField(_("Status"), max_length=7, choices=STATUS_CHOICES, default=None, null=True)
     requires_registration = models.BooleanField(null=True, blank=False)
     usk = models.CharField(_("Altersfreigabe"), max_length=5, choices=USK_CHOICES, null=True, blank=True)
-    pro = ArrayField(models.CharField(max_length=200), verbose_name=_("Pro"), default=list, null=True, blank=True)
-    contra = ArrayField(models.CharField(max_length=200), verbose_name=_("Kontra"), default=list, null=True, blank=True)
+    pro = ArrayField(models.CharField(max_length=300), verbose_name=_("Pro"), default=list, null=True, blank=True)
+    contra = ArrayField(models.CharField(max_length=300), verbose_name=_("Kontra"), default=list, null=True, blank=True)
     privacy = models.IntegerField(_("Datenschutz"), choices=PRIVACY_CHOICES, null=True, blank=True)
     description = models.TextField(_("Beschreibung"), null=True, blank=True)
     usage = models.TextField(_("Nutzung"), null=True, blank=True)
@@ -483,9 +483,9 @@ class Trend(Content):
     language = models.CharField(_("Sprache"), max_length=10, choices=LANGUAGE_CHOICHES, blank=True, null=True)
     licence = models.IntegerField(_("Lizenz"), choices=LICENCE_CHOICES, blank=True, null=True)
     category = models.IntegerField(_("Kategorie"), choices=CATEGORY_CHOICES, blank=True, null=True)
-    target_group = ArrayField(models.CharField(max_length=200), verbose_name=_("Zielgruppe"), default=list, null=True,
+    target_group = ArrayField(models.CharField(max_length=300), verbose_name=_("Zielgruppe"), default=list, null=True,
                               blank=True)
-    publisher = ArrayField(models.CharField(max_length=200), verbose_name=_("Herausgeber"), default=list, null=True,
+    publisher = ArrayField(models.CharField(max_length=300), verbose_name=_("Herausgeber"), default=list, null=True,
                            blank=True)
     publisher_date = models.DateField(_("Datum der Veröffentlichung"), blank=True, null=True)
     central_contents = models.TextField(_("Zentrale Inhalte"), blank=True, null=True)
