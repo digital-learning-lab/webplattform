0071f0340bb71e087d8357431a6c7a28414c9298
Robert Stein <robert@blueshoe.de>
2020-01-13 14:27:02 +0100

    - set pagination to 20 - better boost for name - remove filterable from vue-select - update vue-select

--- dll/configuration/settings.py
+++ dll/configuration/settings.py
@@ -246,7 +246,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
     'SEARCH_PARAM': 'q',
     'DEFAULT_PERMISSION_CLASSES': []
 }
