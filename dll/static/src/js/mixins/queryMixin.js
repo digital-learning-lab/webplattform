7ee6bed3cd4fc4024149f4b4010451ebe6fd80a7
Robert Stein <robert@blueshoe.de>
2020-04-27 17:54:12 +0200

    - add query string processing to competence filter

--- dll/static/src/js/mixins/queryMixin.js
+++ dll/static/src/js/mixins/queryMixin.js
@@ -10,8 +10,8 @@ export const queryMixin = {
   },
   methods: {
     updateQueryString () {
-      let params = this.getParams()
-      Object.keys(params).forEach((key) => (!params[key]) && delete params[key]);
+      let params = this.getParams(this.currentPage)
+      Object.keys(params).forEach((key) => (params[key] === null || params[key] === "") && delete params[key]);
       if (params) {
         window.history.pushState(
           {},
@@ -22,11 +22,19 @@ export const queryMixin = {
     }
   },
   created () {
-    const query = queryString.parse(location.search);
+    const query = queryString.parse(location.search, {
+      parseBooleans: true
+    });
     let keys = Object.keys(query)
     for (let i = 0; i < keys.length; i++) {
       const value = query[keys[i]]
       if (value) {
+        if (query[keys[i]] === 'false') {
+          query[keys[i]] = false
+        }
+        if (query[keys[i]] === 'true') {
+          query[keys[i]] = true
+        }
         if (this[keys[i]] && Array.isArray(this[keys[i]]) && !Array.isArray(query[keys[i]])) {
           this[keys[i]] = [query[keys[i]]]
         } else {
