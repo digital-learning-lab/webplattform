f40f9413d9ea175f3680f004d0c39892cd07bcbf
Robert Stein <robert@blueshoe.de>
2020-06-03 09:59:56 +0200

    - fixes js error on non-recaptcha pages

--- dll/static/src/js/recaptcha.js
+++ dll/static/src/js/recaptcha.js
@@ -1,8 +1,10 @@
 import $ from 'jquery'
 $(document).ready(function () {
-    grecaptcha.ready(function () {
-        grecaptcha.execute(websiteKey, {action: 'generate_link'}).then(function (token) {
-            $('[name="g-recaptcha-response"]').val(token);
+    if (typeof grecaptcha !== 'undefined') {
+        grecaptcha.ready(function () {
+            grecaptcha.execute(websiteKey, {action: 'generate_link'}).then(function (token) {
+                $('[name="g-recaptcha-response"]').val(token);
+            });
         });
-    });
+    }
 })
