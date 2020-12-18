f604b6d27534051480f64164c95920e3d82316e8
Robert Stein <robert@blueshoe.de>
2020-12-18 10:19:54 +0100

    #153 Suchmaske für das Verwalten der User - add search_fields to user admin

--- dll/user/admin.py
+++ dll/user/admin.py
@@ -4,6 +4,7 @@ from .models import DllUser
 
 class DllUserAdmin(admin.ModelAdmin):
     exclude = ("json_data",)
+    search_fields = ["first_name", "last_name", "username", "email"]
 
 
 admin.site.register(DllUser, DllUserAdmin)
