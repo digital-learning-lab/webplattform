067399711291cf2b81ede76e8b98b14b3e54a018
Robert Stein <robert@blueshoe.de>
2021-02-02 21:06:09 +0100

    feature: survey export
    - update django-import-export
    - add survey export

--- dll/survey/admin.py
+++ dll/survey/admin.py
@@ -1,6 +1,9 @@
+import xlsxwriter
 from django.contrib import admin
 
 import nested_admin
+from django.http import HttpResponse
+from django.utils.six import BytesIO
 
 from dll.survey.models import (
     Trigger,
@@ -32,7 +35,46 @@ class SurveyQuestionInlineAdmin(nested_admin.NestedStackedInline):
 
 
 class SurveyAdmin(nested_admin.NestedModelAdmin):
+    actions = ["export_xlsx"]
     inlines = [SurveyQuestionInlineAdmin]
+
+    def export_xlsx(self, request, queryset):
+        output = BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet()
+
+        survey = queryset.get()
+
+        questions = [question.title for question in survey.survey_questions.all()]
+
+        worksheet.write_row(0, 0, questions)
+        for row, result in enumerate(
+            SurveyResult.objects.filter(survey=survey), start=1
+        ):
+            k = []
+            for question in survey.survey_questions.all():
+                value = ""
+                try:
+                    value = SurveyResultAnswer.objects.get(
+                        result_id=result.pk, question_id=question.pk
+                    ).value
+                except:
+                    pass
+                k.append(value)
+
+            worksheet.write_row(row, 0, k)
+        workbook.close()
+        output.seek(0)
+
+        response = HttpResponse(
+            output,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        response["Content-Disposition"] = "attachment; filename=umfrageergebnisse.xlsx"
+
+        return response
+
+    export_xlsx.short_description = "Umfrageergebnisse exportieren"
 
 
 class SurveyResultAnswerInlineAdmin(admin.StackedInline):
