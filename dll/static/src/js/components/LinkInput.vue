c3ee5adc9904f5a367ef5ddcf87d22d7ce7bebe1
Robert Stein <robert@blueshoe.de>
2021-02-15 22:05:41 +0100

    fix: loosen url validation
    - loosen url validation due to false positives, as well as excessive CPU usage (in some cases)

--- dll/static/src/js/components/LinkInput.vue
+++ dll/static/src/js/components/LinkInput.vue
@@ -88,13 +88,7 @@
     },
     methods: {
       isValidUrl (url) {
-        // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
-        var pattern = new RegExp('^(https?:\\/\\/)'+
-          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
-          '((\\d{1,3}\\.){3}\\d{1,3}))'+
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
-          '(\\?[;&a-z\\d%_.~+=-]*)?'+
-          '(\\#[-a-z\\d_]*)?$','i');
+        var pattern = new RegExp(/^(ftp|http|https):\/\/[^ "]+$/,'i');
         return !!pattern.test(url)
       },
       checkLinkValid (link) {
@@ -114,7 +108,6 @@
     watch: {
       internalLink: {
         handler: function (newValue) {
-           console.log(newValue)
           this.$emit('update:link', newValue)
         },
         deep: true
