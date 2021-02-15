a8faa51effd75c644b7247aa25d1b6d8a8087467
Robert Stein <robert@blueshoe.de>
2021-02-15 21:52:17 +0100

    feature: sharing track url
    - add location href to tracking for social sharing buttons

--- dll/static/src/js/sharing.js
+++ dll/static/src/js/sharing.js
@@ -8,6 +8,6 @@ $(function () {
   });
   $("body").on("click", "[data-share]", (e) => {
     var value = $(e.target).closest("[data-share]").data("share");
-    _paq.push(["trackEvent", "Sharing", value]);
+    _paq.push(["trackEvent", "Sharing", value, window.location.href]);
   });
 });
