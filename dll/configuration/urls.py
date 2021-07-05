a995295bea02da89ccfcc5c0459a18233bfe268a
Robert Stein <robert@blueshoe.de>
2021-07-05 15:03:37 +0200

    fix: remove unused import

--- dll/configuration/urls.py
+++ dll/configuration/urls.py
@@ -16,18 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.flatpages import views
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from dll.content.views import (
-    HomePageView,
-    StructureView,
-    UsageView,
-    DevelopmentView,
     ToolDetailView,
     TrendDetailView,
     TeachingModuleDetailView,
