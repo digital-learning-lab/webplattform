978e01dc18dc78aad02caadeffa4d8dccc376bd9
Georg Krause <georg@blueshoe.de>
2022-05-18 08:43:03 +0200

    Add default values to some constance settings

--- dll/configuration/settings.py
+++ dll/configuration/settings.py
@@ -532,9 +532,9 @@ CONSTANCE_CONFIG = {
         "Dankes-Nachricht bei Umfragen",
         str,
     ),
-    "GUEST_MENU_HANDLE": ("guest_menu", str),
-    "LOGGED_IN_MENU_HANDLE": ("logged_in_menu", str),
-    "REVIEWER_MENU_HANDLE": ("reviewer_menu", str),
+    "GUEST_MENU_HANDLE": ("", "guest_menu", str),
+    "LOGGED_IN_MENU_HANDLE": ("", "logged_in_menu", str),
+    "REVIEWER_MENU_HANDLE": ("", "reviewer_menu", str),
 }
 
 WAGTAIL_SITE_NAME = "digital.learning.lab"
