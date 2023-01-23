a7905ce9f724c8c86e7c773b920119d77b1ae5d2
Robert Stein <robert@blueshoe.de>
2023-01-23 17:28:25 +0100

    chore: add footer link
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- dll/general/context_processors.py
+++ dll/general/context_processors.py
@@ -14,18 +14,28 @@ def platform_variables(request):
         "logo_mobile_2x": "img/logo/dll_logo_rgb_ohne_claim_large.png",
         "SITE_ID": 1,
     }
-    if site.id == 1:
-        return DEFAULTS
-    elif site.id == 2:
-        return {
-            "main_js": "main_dlt",
-            "main_css": "main_dlt",
-            "template_suffix": "dlt",
-            "title": "digital.learning.tools",
-            "logo_desktop_1x": "img/logo/logo_dlt.svg",
-            "logo_desktop_2x": "img/logo/logo_dlt.svg",
-            "logo_mobile_1x": "img/logo/logo_dlt_mobile.svg",
-            "logo_mobile_2x": "img/logo/logo_dlt_mobile.svg",
-            "SITE_ID": 2,
-        }
+    try:
+        DEFAULTS.update(
+            {
+                "dll_url": f"https://{Site.objects.get(id=1).domain}",
+                "dlt_url": f"https://{Site.objects.get(id=2).domain}",
+            }
+        )
+    except Site.DoesNotExist:
+        pass
+
+    if site.id == 2:
+        DEFAULTS.update(
+            {
+                "main_js": "main_dlt",
+                "main_css": "main_dlt",
+                "template_suffix": "dlt",
+                "title": "digital.learning.tools",
+                "logo_desktop_1x": "img/logo/logo_dlt.svg",
+                "logo_desktop_2x": "img/logo/logo_dlt.svg",
+                "logo_mobile_1x": "img/logo/logo_dlt_mobile.svg",
+                "logo_mobile_2x": "img/logo/logo_dlt_mobile.svg",
+                "SITE_ID": 2,
+            }
+        )
     return DEFAULTS
