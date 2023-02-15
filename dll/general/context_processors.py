abdb62f3be2a41c68923bd7e8b2b5b642404849e
Robert Stein <robert@blueshoe.de>
2023-02-15 16:50:40 +0100

    chore: add discover more section to tool detail page
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- dll/general/context_processors.py
+++ dll/general/context_processors.py
@@ -8,6 +8,7 @@ def platform_variables(request):
         "main_css": "main_dll",
         "template_suffix": "dll",
         "title": "digital.learning.lab",
+        "dll_logo": "img/logo/dll_logo_rgb_claim_rechts.png",
         "logo_desktop_1x": "img/logo/dll_logo_rgb_claim_rechts.png",
         "logo_desktop_2x": "img/logo/dll_logo_rgb_claim_rechts_large.png",
         "logo_mobile_1x": "img/logo/dll_logo_rgb_ohne_claim.png",
