8d324554f20b4382b7acd58858768a828a6d37c9
Robert Stein <robert@blueshoe.de>
2023-02-16 22:38:06 +0100

    chore: better default email
    Signed-off-by: Robert Stein <robert@blueshoe.de>

--- dll/communication/migrations/0008_review_comm_type.py
+++ dll/communication/migrations/0008_review_comm_type.py
@@ -4,11 +4,15 @@ from django.db import migrations
 def forward_func(apps, schema_editor):
     CommunicationEventType = apps.get_model("communication", "CommunicationEventType")
     CommunicationEventType.objects.create(
-        code="TESTIMONIAL_SUBMITTED_FOR_REVIEW", name="Erfahrungsbericht eingericht"
+        code="TESTIMONIAL_SUBMITTED_FOR_REVIEW",
+        name="Erfahrungsbericht eingericht",
+        from_email="kontakt@digitallearninglab.de",
     )
 
     CommunicationEventType.objects.create(
-        code="TESTIMONIAL_REVIEW_DONE", name="Erfahrungsbericht aktualisiert"
+        code="TESTIMONIAL_REVIEW_DONE",
+        name="Erfahrungsbericht aktualisiert",
+        from_email="kontakt@digitallearninglab.de",
     )
 
 
